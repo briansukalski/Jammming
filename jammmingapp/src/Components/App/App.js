59bc9d1e442c5867e859a37ebccc66d27bdac02b
briansukalski <sukalskibrian@gmail.com>
2022-10-04 18:06:21 -0500

    Set up directory structure of app and general render structure of App.js

---
+++ jammmingapp/src/Components/App/App.js
@@ -0,0 +1,22 @@
+// import logo from './logo.svg';
+import { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+              {/* Add a SearchBar component */}
+          <div className="App-playlist">
+              {/* Add a SearchResultsComponent */}
+              {/* Add a Playlist Component */}
+          </div>
+        </div>  
+      </div>
+    );
+  }
+} 
+
+export default App;
