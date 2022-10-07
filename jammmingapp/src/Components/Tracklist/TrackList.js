6b149404d4f21cb90de1f4b86ac00eb3bdf643a2
briansukalski <sukalskibrian@gmail.com>
2022-10-06 20:20:49 -0500

    Set up structure and style of TrackList component

---
+++ jammmingapp/src/Components/Tracklist/TrackList.js
@@ -0,0 +1,12 @@
+import {Component} from 'react';
+import './TrackList.css';
+
+export class TrackList extends Component {
+    render() {
+        return (
+            <div className="TrackList">
+                {/*Will contain a map method rendering a set of Track components*/}
+            </div>
+        );
+    }
+}
