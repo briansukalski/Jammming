47f577b950b80c0c25ec1667dff73e40d663979b
briansukalski <sukalskibrian@gmail.com>
2022-10-25 20:13:51 -0500

    Set up shells of methods to save playlist and search for tracks; Spotify API to be connected later

--- jammmingapp/src/Components/App/App.js
+++ jammmingapp/src/Components/App/App.js
@@ -56,6 +56,8 @@ class App extends Component {
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
     this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
+    this.search = this.search.bind(this);
   }
 
   render() {
@@ -63,10 +65,10 @@ class App extends Component {
       <div>
         <h1>Ja<span className="highlight">mmm</span>ing</h1>
         <div className="App">
-              <SearchBar/>
+              <SearchBar onSearch={this.search}/>
           <div className="App-playlist">
               <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack}/>
-              <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} onNameChange={this.updatePlaylistName}/>
+              <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} onNameChange={this.updatePlaylistName} onSave={this.savePlaylist}/>
           </div>
         </div>  
       </div>
@@ -101,6 +103,14 @@ class App extends Component {
 
   updatePlaylistName(name) {
     this.setState({playlistName: name});
+  }
+
+  savePlaylist() {
+    const trackURIs = this.state.playlistTracks.map(playlistTrack => playlistTrack.id);
+  }
+
+  search(searchTerm) {
+    alert(searchTerm);
   }
 
 } 
--- jammmingapp/src/Components/SearchBar/SearchBar.js
+++ jammmingapp/src/Components/SearchBar/SearchBar.js
@@ -2,12 +2,27 @@ import { Component } from 'react';
 import './SearchBar.css';
 
 export class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {searchTerm: null};
+        this.search = this.search.bind(this);
+        this.handleTermChange = this.handleTermChange.bind(this);
+    }
+
     render() {
         return (
             <div className="SearchBar">
-                <input placeholder="Enter A Song, Album, or Artist"/>
+                <input placeholder="Enter A Song, Album, or Artist" onChange={this.handleTermChange}/>
                 <button className="SearchButton">SEARCH</button>
             </div>
         );
+    }
+
+    search(searchTerm) {
+        this.props.onSearch(searchTerm);
+    }
+
+    handleTermChange(event) {
+        this.setState({searchTerm: event.target.value});
     }
 }
